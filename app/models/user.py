312f1d8c9b9d96fef274b6221133c56bfe8c4ebf
gdhaison <vuhaisonhb456@gmail.com>
2025-06-12 16:09:26 +0700

    add table name

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from .. import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
